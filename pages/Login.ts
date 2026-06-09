0a7f8aa5728baec69af6781ed1fd368bad311fab
Andi Efendi <andiefendi.id@gmail.com>
2026-06-09 13:06:47 +0700

    workflow file

--- pages/Login.ts
+++ pages/Login.ts
@@ -7,6 +7,9 @@ export class LoginPage {
     readonly signInButton: Locator;
     readonly validateSuccessMessage: Locator;
     readonly validateErrorMessage: Locator;
+    readonly menuProfile: Locator;
+    readonly buttonLogout: Locator;
+    readonly validateLogoutSuccess: Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -15,16 +18,25 @@ export class LoginPage {
         this.signInButton = page.getByRole('button', { name: 'Sign In' });
         this.validateSuccessMessage = page.getByRole('button', { name: 'Home' });
         this.validateErrorMessage = page.getByText('Invalid credentials');
+
+        this.menuProfile = page.getByRole('button', { name: 'Andi Efendi andiemra@yopmail.' });
+        this.buttonLogout = page.getByRole('menuitem', { name: 'Logout' });
+        this.validateLogoutSuccess = page.getByText('Welcome Back');
+    
     }
 
     async goto() {
         await this.page.goto('https://www.emra.chat/login');
-    }
+    };
 
     async login(email: string, password: string) {
         await this.emailInput.fill(email);
         await this.passwordInput.fill(password);
         await this.signInButton.click();
-    }
+    };
 
+    async logout() {
+        await this.menuProfile.click();
+        await this.buttonLogout.click();
+    };
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,21 +1,25 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/Login';
-
-test('unsuccessful login using ', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-
-  await loginPage.goto();
-  await loginPage.login('andiemral@myopmail.com', '12345');
-
-  await expect(loginPage.validateErrorMessage).toBeVisible();
-})
+import {test, expect} from '@playwright/test';
+import {LoginPage} from '../pages/Login';
 
 test('successful login', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.goto();
   await loginPage.login('andiemra@yopmail.com', '1234567890');
-
   await expect(loginPage.validateSuccessMessage).toBeVisible();
+  await loginPage.logout();
+  await expect(loginPage.validateLogoutSuccess).toBeVisible();
 });
+
+test('unsuccessful login using ', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.login('andiemral@myopmail.com', '12345');
+  await expect(loginPage.validateErrorMessage).toBeVisible();
+});
+
+
+
+
 
